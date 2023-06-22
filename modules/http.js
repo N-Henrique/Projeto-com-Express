9e9c6d98d83336f8c2738b0af41a1fb4f41768c8
Henrique <n.n.henrique.n.n@gmail.com>
2023-06-22 15:25:05 -0300

    criando servidor

--- modules/http.js
+++ modules/http.js
@@ -1,11 +1,29 @@
+//Cria um servidor
 const http = require("http");
 
 const port = 8080;
 
-const sever = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   if (req.url === "/home") {
-    res.whiteHead(200, { "Content-Tyoe": "text/html" });
+    //enviar HTLM
+    res.writeHead(200, { "Content-Tyoe": "text/html" });
     res.end("<h1>home page</h1>");
   }
+  //Pegar a URL da request
+  if (req.url === "/users") {
+    const users = [
+      {
+        name: "Henrique",
+        email: "Henrique@email",
+      },
+      {
+        name: "jessica",
+        email: "jessica@email",
+      },
+    ];
+    //enviar JSON
+    res.writeHead(200, { "Content-Type": "applicarion/json" });
+    res.end(JSON.stringify(users));
+  }
 });
-Server.listen(port, () => console.log(`Rodando na porta ${port}!`));
+server.listen(port, () => console.log(`Rodando na porta ${port}!`));
